dc9118d6e38cd7507d69f939404f8aef095dfd22
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-03-23 11:11:38 -0300

    Criado componente ista-compras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalculadoraComponent } from "./calculadora/calculadora.component";
 import { CalcFormComponent } from "./calc-form/calc-form.component";
+import { ListaComprasComponent } from './lista-compras/lista-compras.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CalculadoraComponent, CalcFormComponent],
+  imports: [RouterOutlet, CalculadoraComponent, CalcFormComponent, ListaComprasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
