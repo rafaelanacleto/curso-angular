fee287423e2bf9d544185d676165b0c1011bf050
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-03-22 21:52:37 -0300

    Criado segundo componente pela linah de comando cli: ng generate c page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloWorldComponent } from "./helloworld/helloworld.component";
+import { MinhaPaginaComponent } from "./minha-pagina/minha-pagina.component"
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloWorldComponent],
+  imports: [RouterOutlet, HelloWorldComponent, MinhaPaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
