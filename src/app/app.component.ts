4cf53ae4dde4f1a8ea0446e2aeb992cc4f56eab0
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-03-23 08:59:22 -0300

    Ajuste css no componente calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HelloWorldComponent } from "./helloworld/helloworld.component";
-import { MinhaPaginaComponent } from "./minha-pagina/minha-pagina.component";
 import { CalculadoraComponent } from "./calculadora/calculadora.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloWorldComponent, MinhaPaginaComponent, CalculadoraComponent],
+  imports: [RouterOutlet, CalculadoraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
